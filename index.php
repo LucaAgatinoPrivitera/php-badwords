b64e3ccdc9ef747f1ab401dd5a050994c5bdbd3a
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-06-10 14:56:12 +0200

    Ho creato il readme, e assegnato un valore al $prova

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
+<?php
+$prova = "ccc";
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-badwords</title>
 </head>
+
 <body>
-    
+    <h1><?php echo $prova; ?></h1>
 </body>
+
 </html>
